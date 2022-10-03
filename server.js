b54aab7bce5f777886824cbe530cf8344f02097e
Tabea Schneider <abeat@posteo.net>
2022-10-03 22:28:11 +0200

    add server structure & html & signature script to petition project [incomplete]

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const db = require("./db");
 const express = require("express");
+const path = require("path");
 const { urlencoded } = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
@@ -8,15 +9,56 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/static", express.static(path.join(__dirname, "static")));
 const PORT = 8080;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+    if (req.cookies.signed) {
+        res.redirect("/thanks");
+    } else {
+        res.redirect("/sign");
+    }
+});
 
-app.get("/sign", (req, res) => {});
+app.get("/sign", (req, res) => {
+    if (req.cookies.signed) {
+        res.redirect("/thanks");
+    } else {
+        res.render("form", {
+            title: "Sign the petition",
+            script: "/static/canvas.js",
+            layout: "main",
+            data: {
+                imagelink:
+                    "https://dictionary.cambridge.org/de/images/full/tree_noun_001_18152.jpg?version=5.0.252",
+                imagedescription: "tree",
+                campaigntext: "Save Berlin's Trees!",
+                description: "Help prevent the dying of trees.",
+            },
+        });
+    }
+});
 
-app.post("/sign", (req, res) => {});
+app.post("/sign", (req, res) => {
+    if (req.body.firstname && req.body.lastname && req.body.signature) {
+        res.cookie("signed", true);
+    }
+    db.createSignatureEntry(
+        req.body.firstname,
+        req.body.lastname,
+        req.body.signature
+    );
+});
 
-app.get("/thanks", (req, res) => {});
+app.get("/thanks", (req, res) => {
+    if (!req.cookies.signed) {
+        res.redirect("/sign");
+    } else {
+        res.render("thanks", {
+            title: "Save Berlin's Trees",
+        });
+    }
+});
 
 app.get("/signatures", (req, res) => {});
 
---
+++ static/canvas.js
@@ -0,0 +1,57 @@
+(() => {
+    const canvas = document.getElementById("canvas");
+    const signature = document.getElementById("signature");
+    var ctx = canvas.getContext("2d");
+    let done = false;
+    let cp1x = 0;
+    let cp1y = 0;
+    let dataURL;
+
+    function drawSignature(cp1x, cp1y, cp2x, cp2y, x, y) {
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+        ctx.stroke();
+        // dataURL = canvas.toDataURL();
+        // signature.value = dataUrl;
+    }
+
+    canvas.addEventListener("mouseup", () => {
+        done = true;
+    });
+    canvas.addEventListener("mousedown", (e) => {
+        done = false;
+        cp1x = e.offsetX;
+        cp1y = e.offsetY;
+        findPoints();
+    });
+
+    function findPoints() {
+        setTimeout(() => {
+            canvas.addEventListener(
+                "mousemove",
+                (e) => {
+                    const cp2x = e.offsetX;
+                    const cp2y = e.offsetY;
+                    setTimeout(() => {
+                        addEventListener(
+                            "mousemove",
+                            (e) => {
+                                const x = e.offsetX;
+                                const y = e.offsetY;
+                                done === false &&
+                                    drawSignature(cp1x, cp1y, cp2x, cp2y, x, y);
+                                cp1x = x;
+                                cp1y = y;
+                                done === false && findPoints();
+                            },
+                            { once: true }
+                        );
+                    }, 50);
+                },
+                { once: true }
+            );
+        }, 50);
+    }
+})();
